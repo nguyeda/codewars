366018a8e88307e9a72bbb3caf9aa5963c393269
David Nguyen <david@netis.be>
2024-04-15 09:22:39 +0200

    :memo: add kata info

--- continued_fraction/main.py
+++ continued_fraction/main.py
@@ -1,4 +1,14 @@
-# it should return the continued fraction of de/nu in an array
+"""
+In this kata, you will have to return the continued fractionwiki of a fraction.
+
+For example, if the numerator is 311 and the denominator is 144, then you would have to return [2, 6, 3, 1, 5], because:
+
+311 / 144 = 2 + 1/(6 + 1/(3 + 1/(1 + 1/5)))
+
+If the numerator is 0, you should return [].
+"""
+
+
 def continued_fraction(nu: int, de: int) -> list[int]:
     def generator(n, d):
         while n > 0 and d > 0:
--- multiples_3_5/main.py
+++ multiples_3_5/main.py
@@ -1,3 +1,15 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
+multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+"""
+
+
 def solution(number: int) -> int:
     fizz = set(range(3, number, 3))
     buzz = set(range(5, number, 5))
